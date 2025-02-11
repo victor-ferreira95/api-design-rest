349c87185d93d3b4516e5d835259123dad111181
victor_ferreira <vitorsantosferreira10@gmail.com>
2025-02-10 21:35:24 -0300

    feat: add cache controll for web

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from 'express';
 import { createCategoryService } from '../services/category.service';
+import cors  from 'cors';
+import { defaultCorsOptions } from '../http/cors';
 
 const router = Router();
 
-router.get('/:categorySlug', async (req, res) => {
+const corsItem = cors({
+    ...defaultCorsOptions,
+    methods: ['GET',]
+  });
+
+
+const corsCollections = cors({
+  ...defaultCorsOptions,
+  methods: ['GET',]
+});
+
+router.get('/:categorySlug', corsItem, async (req, res) => {
     const categoryService = await createCategoryService();
     const category = await categoryService.getCategoryBySlug(req.params.categorySlug);
     res.json(category);
 });
 
-router.get('/', async (req, res) => {
+router.get('/', corsCollections, async (req, res) => {
     const categoryService = await createCategoryService();
     const { page = 1, limit = 10, name } = req.query;
     const { categories, total } = await categoryService.listCategories({
@@ -17,7 +30,13 @@ router.get('/', async (req, res) => {
         limit: parseInt(limit as string),
         filter: { name: name as string }
     });
+
+    res.set("Cache-Control", "public, max-age=30")
     res.json({ categories, total });
 });
+
+router.options("/:categorySlug", corsItem);
+router.options("/", corsCollections);
+
 
 export default router;
